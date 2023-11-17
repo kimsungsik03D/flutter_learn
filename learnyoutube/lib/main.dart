621a8f4a1896965bb8108802b8a3f245d4f4ac09
sungsik:D <tjdtlr2620@gmail.com>
2023-11-17 12:52:42 +0900

    Ch4 button

--- learnyoutube/lib/main.dart
+++ learnyoutube/lib/main.dart
@@ -49,37 +49,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: const Text("text Title")),
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.grey,
-        child: Stack(
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 50, left: 50),
-              color: Colors.red,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 100, left: 100),
-              color: Colors.green,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.only(top: 150, left: 150),
-              color: Colors.orange,
-            )
-          ],
+          child: Row(children: [
+        GestureDetector(
+          onTap: () => print("GestureDetector"),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+          ),
         ),
-      ),
+        TextButton(
+            onPressed: () => print("TextButton"), child: Text("TextButton"))
+      ])),
     );
   }
 }
