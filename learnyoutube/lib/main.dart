e1ec13904641539176a785e6a82275787944169f
sungsik:D <tjdtlr2620@gmail.com>
2023-11-28 01:19:32 +0900

    ch.26 getX ( part 1 )

--- learnyoutube/lib/main.dart
+++ learnyoutube/lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import './home.dart';
+import 'package:get/get.dart';
+import 'package:learnyoutube/countController.dart';
 
 void main() {
-  const ProviderScope(child: MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "RiverPod Example",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("GetX Example")),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            GetBuilder<CounterController>(
+              init: CounterController(),
+              builder: (_) => Text(
+                  "Current Counter: ${Get.find<CounterController>().counter}"),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () => Get.find<CounterController>().increase(),
+                    child: const Text("증가")),
+                TextButton(
+                    onPressed: () => Get.find<CounterController>().decrease(),
+                    child: const Text("김소"))
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
 }
